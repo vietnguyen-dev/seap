f433bd47d4f61c60cfe1aaf0583e4a98b00587db
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-02-22 12:52:38 -0800

    httpOnly and secure in session affectng prod

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,8 @@ app.use(session({
   store:  new RedisStore({ client: redisClient as any}),
   saveUninitialized: false,
   cookie: { 
-
-    httpOnly:  false,
-    secure:  false,
+    httpOnly: false,
+    secure: environment ? true : false,
     //1000 ms * 60 seconds * 60 minutes * 24 hours
     maxAge: 1000 * 60 * 60 * 24 
   },
