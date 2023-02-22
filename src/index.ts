40eeb557aea6bf731d2af76b1021a82db36a4d2e
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-02-22 13:19:38 -0800

    proxy flag in session may help?

--- src/index.ts
+++ src/index.ts
@@ -36,9 +36,10 @@ app.use(session({
   resave: false,
   store:  new RedisStore({ client: redisClient as any}),
   saveUninitialized: false,
+  proxy: true,
   cookie: { 
     httpOnly: environment ? true : false,
-    secure: false,
+    secure: environment ? true : false,
     //1000 ms * 60 seconds * 60 minutes * 24 hours
     maxAge: 1000 * 60 * 60 * 24 
   },
